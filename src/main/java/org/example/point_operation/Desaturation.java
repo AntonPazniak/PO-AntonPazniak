e441df6288fd4d986b2064da8082fe246895125e
Anton <poznyak969@gmail.com>
2024-03-24 12:20:18 +0100

    minor fix Desaturation

--- src/main/java/org/example/point_operation/Desaturation.java
+++ src/main/java/org/example/point_operation/Desaturation.java
@@ -6,17 +6,21 @@ public class Desaturation {
 
     public static void convert(PortableAnymap portableAnymap) {
         if (portableAnymap.getHead().equals("P3")) {
-            convertP3(portableAnymap);
+            convertP3(portableAnymap.getMatrix());
+            portableAnymap.updateImage();
         }
     }
 
-    public static void convertP3(PortableAnymap portableAnymap) {
-        for (int i = 0; i < portableAnymap.getHeight(); i++) {
-            for (int j = 0; j < portableAnymap.getWidth(); j++) {
-                portableAnymap.getMatrix()[i][j][1] = portableAnymap.getMatrix()[i][j][0];
-                portableAnymap.getMatrix()[i][j][2] = portableAnymap.getMatrix()[i][j][0];
+
+    public static void convertP3(int[][][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int R = matrix[i][j][0];
+                int G = matrix[i][j][1];
+                int B = matrix[i][j][2];
+                int Y = (int) (0.2126 * R + 0.7152 * G + 0.0722 * B);
+                matrix[i][j] = new int[]{Y, Y, Y};
             }
         }
-        portableAnymap.updateImage();
     }
 }
