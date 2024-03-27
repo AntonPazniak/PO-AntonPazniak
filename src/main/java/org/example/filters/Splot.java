622fe6f749a5185abb5fb370ba5f0478ee023131
Anton <poznyak969@gmail.com>
2024-03-27 10:52:22 +0100

    update splot

--- src/main/java/org/example/filters/Splot.java
+++ src/main/java/org/example/filters/Splot.java
@@ -2,24 +2,24 @@ package org.example.filters;
 
 public class Splot {
 
-    private static float[][] sobel = {
+    private static final float[][] sobel = {
             {1, 0, -1},
             {2, 0, -2},
             {1, 0, -1}
     };
 
-    private static float[][] roberts = {
+    private static final float[][] roberts = {
             {1, 0},
             {0, -1}
     };
 
-    private static float[][] laplace = {
+    private static final float[][] laplace = {
             {0, 1, 0},
             {1, -4, 1},
             {0, 1, 0}
     };
 
-    private static float[][] LoG = {
+    private static final float[][] LoG = {
             {0, 0, -1, 0, 0},
             {0, -1, -2, -1, 0},
             {-1, -2, 16, -2, -1},
@@ -27,7 +27,7 @@ public class Splot {
             {0, 0, -1, 0, 0}
     };
 
-    private static float[][] previt = {
+    private static final float[][] previt = {
             {1, 0, -1},
             {1, 0, -1},
             {1, 0, -1}
@@ -54,46 +54,57 @@ public class Splot {
     }
 
     public static void test(float[][] splotMatrix, int[][][] imageMatrix) {
-        int[][][] outputMatrix = new int[imageMatrix.length][imageMatrix[0].length][3];
-
-        for (int x = 0; x < imageMatrix.length; x++) {
-            for (int y = 0; y < imageMatrix[x].length; y++) {
-                float red = 0;
+        int startPos = splotMatrix.length / 2;
+        int sizeSplot = splotMatrix.length * splotMatrix[0].length;
+        int[][][] expandMatrix = expandMatrix(imageMatrix, startPos);
+        for (int x = startPos; x < expandMatrix.length - 1 - startPos; x++) {
+            for (int y = startPos; y < expandMatrix[0].length - 1 - startPos; y++) {
+                float read = 0;
                 float green = 0;
                 float blue = 0;
-                int n = splotMatrix.length * splotMatrix[0].length;
-                for (int z = 0; z < splotMatrix.length; z++) {
-                    for (int r = 0; r < splotMatrix[z].length; r++) {
-                        int indexRow = x - splotMatrix.length / 2 + z;
-                        int indexCol = y - splotMatrix[z].length / 2 + r;
-                        if (indexRow >= 0 && indexRow < imageMatrix.length && indexCol >= 0 && indexCol < imageMatrix[x].length) {
-                            red += imageMatrix[indexRow][indexCol][0] * splotMatrix[z][r];
-                            green += imageMatrix[indexRow][indexCol][1] * splotMatrix[z][r];
-                            blue += imageMatrix[indexRow][indexCol][2] * splotMatrix[z][r];
-                        } else {
-                            n = n - 1;
-                        }
+                for (int i = 0; i < splotMatrix.length; i++) {
+                    for (int j = 0; j < splotMatrix[0].length; j++) {
+                        read += splotMatrix[i][j] * expandMatrix[x - startPos + i][y - startPos + j][0];
+                        green += splotMatrix[i][j] * expandMatrix[x - startPos + i][y - startPos + j][1];
+                        blue += splotMatrix[i][j] * expandMatrix[x - startPos + i][y - startPos + j][2];
                     }
                 }
-
-                red /= n;
-                green /= n;
-                blue /= n;
-
-                outputMatrix[x][y][0] = (int) Math.min(255, Math.max(0, red));
-                outputMatrix[x][y][1] = (int) Math.min(255, Math.max(0, green));
-                outputMatrix[x][y][2] = (int) Math.min(255, Math.max(0, blue));
+                int[] color = new int[3];
+                color[0] = (int) Math.min(255, Math.max(0, read / 2));
+                color[1] = (int) Math.min(255, Math.max(0, green / 2));
+                color[2] = (int) Math.min(255, Math.max(0, blue / 2));
+                imageMatrix[x - startPos][y - startPos] = color;
             }
         }
 
-        for (int x = 0; x < imageMatrix.length; x++) {
-            for (int y = 0; y < imageMatrix[x].length; y++) {
-                for (int z = 0; z < 3; z++) {
-                    imageMatrix[x][y][z] = outputMatrix[x][y][z];
-                }
-            }
-        }
     }
 
+    private static int[][][] expandMatrix(int[][][] matrix, int sizeIncrease) {
+        int[][][] newMatrix = new int[matrix.length + sizeIncrease][matrix[0].length + sizeIncrease][3];
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0; j < newMatrix[0].length; j++) {
+                if (i < sizeIncrease || i > newMatrix.length - 1 - sizeIncrease) {
+                    newMatrix[i][j] = new int[]{1, 1, 1};
+                } else if (j < sizeIncrease || j > newMatrix[0].length - 1 - sizeIncrease) {
+                    newMatrix[i][j] = new int[]{1, 1, 1};
+                } else
+                    newMatrix[i][j] = matrix[i][j];
+            }
+        }
+        return newMatrix;
+    }
+
+    private static void print(int[][][] matrix) {
+        for (int[][] i : matrix) {
+            for (int[] j : i) {
+                System.out.print("(");
+                for (int k : j) {
+                    System.out.print(k + " ");
+                }
+                System.out.print(")");
+            }
+            System.out.println();
+        }
+    }
 
 }
